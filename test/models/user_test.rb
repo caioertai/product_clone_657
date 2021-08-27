8307942237eefc81b22e57fc57b7b988aa5e5521
Caio Andrade <andrade@hey.com>
2021-08-27 09:12:55 -0500

    Unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name returns first and last name" do
+    # SETUP
+    user = User.new(first_name: "Jujuba", last_name: "Andrade")
+
+    # EXERCISE
+    users_full_name = user.full_name
+
+    # ASSERT
+    # assert users_full_name == "Jujuba Andrade"
+    assert_equal "Jujuba Andrade", users_full_name
+
+    # TEARDOWN
+  end
+
+  test "#full_name returns properly capitalized name" do
+    # SETUP
+    user = User.new(first_name: "CaIO", last_name: "AnDRade")
+
+    # EXERCISE
+    users_full_name = user.full_name
+
+    # ASSERT
+    assert_equal "Caio Andrade", users_full_name
+
+    # TEARDOWN
+  end
+
+  test "#full_name returns just last name if first is not present" do
+    # SETUP
+    user = User.new(last_name: "Lennon")
+
+    # EXERCISE
+    users_full_name = user.full_name
+
+    # ASSERT
+    assert_equal "Lennon", users_full_name
+
+    # TEARDOWN
+  end
 end
